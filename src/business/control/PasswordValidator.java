80f4b8810b776c1590679adcdf5e05d9b5911ce9
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-05-29 22:42:56 -0300

    Adicionando validação do formato de senha

--- src/business/control/PasswordValidator.java
+++ src/business/control/PasswordValidator.java
@@ -3,14 +3,27 @@ package business.control;
 import exceptions.UserPasswordException;
 import business.model.User;
 
+import java.util.regex.Pattern;
+
 public class PasswordValidator implements IValidator{
-	
+
+	private Pattern containsTwoNumbersPattern;//padrão para ao menos 2 números
+	private Pattern containsWordCharPattern;//padrão para letras
+
+	public PasswordValidator(){
+		containsTwoNumbersPattern = Pattern.compile(".*\\d{2,}.*");
+		containsWordCharPattern = Pattern.compile(".*[a-zA-Z].*");
+	}
+
 	public void validate(User user) throws UserPasswordException
 	{
 		String value = user.getPassword();
+		boolean containsWordChar = containsWordCharPattern.matcher(value).matches();
+		boolean containsTwoNumbers = containsTwoNumbersPattern.matcher(value).matches();
+		boolean formatValid = containsWordChar && containsTwoNumbers;
 		boolean sizeInvalid = value.length() < 8 || value.length() > 20;
 
-		if(sizeInvalid){//condição de senha inválida
+		if(sizeInvalid || !formatValid){//condição de senha inválida
 			throw new UserPasswordException();
 		}
 	}
