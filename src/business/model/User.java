7a43450a7090d7fd007b5269a5892cc9d0307bea
João Paulo Martins <joaopsilvam88@gmail.com>
2021-05-30 13:08:54 -0300

    Camada de visualização

--- src/business/model/User.java
+++ src/business/model/User.java
@@ -5,6 +5,13 @@ public class User {
 	private String login;
 	private String pass;
 
+	public User(){
+		this("", "");
+	}
+	public User(String login, String pass){
+		this.login = login;
+		this.pass = pass;
+	}
 	public String getLogin(){
 		return login;
 	}
--- src/view/UserForm.java
+++ src/view/UserForm.java
@@ -1,8 +1,41 @@
 package view;
 
+import business.control.UserControl;
+import business.model.User;
+
+import javax.swing.*;
+
 public class UserForm {
 
+	UserControl controller = new UserControl();
+
 	public void menu() {
-		
+		String operation = JOptionPane.showInputDialog("Que operação você deseja fazer no sistema?" +
+				"\n[a] Cadastrar usuário\n[b] Verificar um usuário\n[c] Verificar todos os usuários\n" +
+				"[d] Deletar usuário");
+
+		if(operation.equals("a")) {
+			String login = JOptionPane.showInputDialog("Informe o login do usuário:");
+			String password = JOptionPane.showInputDialog("Informe a senha do usuário:");
+			User user = new User(login, password);
+			controller.add(user);
+			JOptionPane.showMessageDialog(null, "Usuário cadastrado");
+		}
+		else if(operation.equals("b")) {
+			controller.listAll();
+			JOptionPane.showMessageDialog(null, "Verificar um usuário");
+		}
+		else if(operation.equals("c")) {
+			String login = JOptionPane.showInputDialog("Informe o login do usuário:");
+			controller.read(login);
+			JOptionPane.showMessageDialog(null, "Verificar todos os usuários");
+		}
+		else if(operation.equals("d")) {
+			String login = JOptionPane.showInputDialog("Informe o login do usuário:");
+			controller.delete(login);
+			JOptionPane.showMessageDialog(null, "Usuário deletado");
+		}else{
+			JOptionPane.showMessageDialog(null, "Informe uma operação válida");
+		}
 	}
 }
