a7672cb977b3f90558c6d9df2d8bdca5f9e371cd
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-05-29 22:42:38 -0300

    Otimizando expressão regular

--- src/business/control/LoginValidator.java
+++ src/business/control/LoginValidator.java
@@ -3,12 +3,20 @@ package business.control;
 import exceptions.UserLoginException;
 import business.model.User;
 
+import java.util.regex.Pattern;
+
 public class LoginValidator implements IValidator{
+
+	private Pattern containsNumberRegex;
+
+	public LoginValidator(){
+		containsNumberRegex = Pattern.compile(".*\\d.*");
+	}
 
 	public void validate(User user) throws UserLoginException
 	{
 		String value = user.getLogin();
-		boolean containsNumber = value.matches(".*\\d.*");
+		boolean containsNumber = containsNumberRegex.matcher(value).matches();
 		boolean sizeInvalid = value.isEmpty() || value.length() > 12;
 
 		if(containsNumber || sizeInvalid){//condição de senha inválida
