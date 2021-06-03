0c036ea1731422032962fbfaa5fa794ef62e90cd
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-06-03 18:03:09 -0300

    Adicionando modelo de documento.

--- src/business/model/Document.java
+++ src/business/model/Document.java
@@ -1,14 +1,29 @@
 package business.model;
 
-public class Document {
-    private String data;
+import java.util.Date;
 
-    public Document(String data){
+public class Document {
+
+    private String data;
+    private String name;
+    private Date postDate;
+
+    public Document(String data, String name, Date date){
+        this.data = data;
+        this.name = name;
         this.data = data;
     }
 
     public Document() {
-        this("");
+        this("", "", new Date());
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
     }
 
     public String getData(){
@@ -17,5 +32,13 @@ public class Document {
 
     public void setData(String data){
         this.data = data;
+    }
+
+    public Date getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(Date postDate) {
+        this.postDate = postDate;
     }
 }
